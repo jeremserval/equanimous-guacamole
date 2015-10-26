1085450405a5d4138f502fc58131302fc13360fb
Jeremie serval <jerryserval@hotmail.com>
2015-10-26 10:44:11 +0100

    ok

--- testGitBranchWithXcode/testGitBranchWithXcode/MasterViewController.m
+++ testGitBranchWithXcode/testGitBranchWithXcode/MasterViewController.m
@@ -23,6 +23,13 @@
 
 @implementation MasterViewController
 
+
+/*
+ 
+ commit dans la branch 2
+ 
+ */
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
